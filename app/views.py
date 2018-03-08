0c21e67bad8e96299245f6949aa445fad5c36279
0xThamer <thamertx0@gmail.com>
2018-03-08 03:29:47 +0300

    add show , edit , delete

--- app/views.py
+++ app/views.py
@@ -17,3 +17,28 @@ def topic_add():
         return redirect(url_for("home"))
     else:
         return render_template("topic_add.html")
+
+
+@app.route("/topic/delete/<int:_id>")
+def topic_delete(_id):
+    post_store.delete(_id)
+    return redirect(url_for("home"))
+
+
+@app.route("/topic/update/<int:_id>", methods=["GET", "POST"])
+def topic_edit(_id):
+    if request.method == "POST":
+        edit_post = post_store.get_by_id(_id)
+        edit_post.title = request.form["title"]
+        edit_post.content = request.form["content"]
+        post_store.update(edit_post)
+        return redirect(url_for("home"))
+    else:
+        edit_post = post_store.get_by_id(_id)
+        return render_template("topic_edit.html", post=edit_post)
+
+
+@app.route("/topic/show/<int:_id>", methods=["GET"])
+def topic_show(_id):
+    post_show = post_store.get_by_id(_id)
+    return render_template("topic_show.html", post=post_show)
