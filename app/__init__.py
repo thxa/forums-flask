65d9ae9b0989fd1034a6153901b59f61af0ce254
0xThamer <thamertx0@gmail.com>
2018-04-08 14:59:08 +0300

    fix database

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 folder_path = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = f"""sqlite:///{os.path.join(folder_path, "my_database.db")}"""
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
@@ -13,7 +14,7 @@ from app import stores, dummy_data
 
 member_store = stores.MemberStore()
 post_store = stores.PostStore()
-#dummy_data.seed_stores(member_store, post_store)
+dummy_data.seed_stores(member_store, post_store)
 
 from app import views
 from app import api
